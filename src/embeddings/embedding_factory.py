0089393f77922a7a91d7d44ee5c3e5e52f27f184
mateomorin <mateo.morin@insee.fr>
2026-04-03 14:32:40 +0000

    Added embedding_factory.py to create embeddings from config

---
+++ src/embeddings/embedding_factory.py
@@ -0,0 +1,30 @@
+import os
+
+from .basic_embedder import BasicEmbedder
+from .async_embedder import AsyncEmbedder
+
+
+def build_embedding(cfg):
+
+    embed_type = cfg["embed"]["type"]
+    model = cfg["embed"]["model"]
+
+    if embed_type == "async":
+        embedder = AsyncEmbedder(
+            base_url=os.environ["EMBEDDING_API_URL"],
+            model=model,
+            api_key=os.environ["EMBEDDING_API_KEY"]
+        )
+
+        return embedder
+
+    elif embed_type == "basic":
+        embedder = BasicEmbedder(
+            base_url=os.environ["EMBEDDING_API_URL"],
+            model=model,
+            api_key=os.environ["EMBEDDING_API_KEY"]
+        )
+
+        return embedder
+
+    raise ValueError("Unknown embedding type")
