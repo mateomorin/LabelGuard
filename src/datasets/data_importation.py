00b00846f9535184a31f5c0b9b1bf0db6aa95da4
mateomorin <mateo.morin@insee.fr>
2026-04-03 13:31:59 +0000

    data_importation ready and tested

--- src/datasets/data_importation.py
+++ src/datasets/data_importation.py
@@ -4,13 +4,91 @@ import numpy as np
 from collections import Counter
 
 
-def fetch_original_data(
-        path: str,
-        n_samples: int,
-        random_state: int = None
-        ) -> pd.DataFrame:
+def get_n_codes(con, path):
     """
-    Fetch the original parquet dataset with columns ["code", "embedding"] from S3.
+    Get the number of distinct codes in the database.
+    """
+
+    code_len_query = f"""
+    SELECT COUNT(DISTINCT(code))
+    FROM read_parquet('{path}')
+    """
+
+    return con.execute(code_len_query).fetchone()[0]
+
+
+def exhaustive_sampling(con, path, rng):
+    """
+    Sample one row per code for every code.
+    Returns the selected row ids and the total number of rows.
+    """
+
+    # Retrieving the number of labels per codes
+    code_row_count = con.execute(f"""
+    SELECT code, COUNT(*) AS n_rows
+    FROM read_parquet('{path}')
+    GROUP BY code
+    ORDER BY code
+    """).df()
+
+    # Sampling
+    code_row_count["row_id"] = code_row_count["n_rows"].apply(
+        lambda n: rng.integers(1, n + 1)
+    )
+    code_row_count["offset"] = code_row_count["n_rows"].cumsum() - code_row_count["n_rows"]
+    code_row_count["row_id"] = code_row_count["offset"] + code_row_count["row_id"]
+
+    return code_row_count["row_id"].to_numpy(), code_row_count["n_rows"].sum()
+
+
+def remaining_sampling(n_samples, n_codes, n_rows, selected_ids, rng):
+    """
+    Sample the remaining codes uniformly to get n_samples samples.
+    selected_ids are the ids that have already been selected and that should not be selected again.
+    """
+    remaining = n_samples - n_codes
+
+    # Adjust if remaining is too high
+    remaining = min(n_rows, remaining)
+
+    # Sample the remaining codes
+    if remaining > 0:
+        remaining_ids = np.delete(np.arange(1, n_rows + 1), selected_ids - 1)
+        random_ids = rng.choice(remaining_ids, size=remaining, replace=False)
+        final_ids = np.concatenate([selected_ids, random_ids])
+    else:
+        final_ids = selected_ids
+
+    return final_ids
+
+
+def retrieve_matching_rows(con, path, selected_ids_table_name):
+    """
+    Retrieve the rows from the database sharing the same ids as in selected_ids.
+    """
+
+    query = f"""
+    SELECT t.code, t.label, t.embedding
+    FROM (
+        SELECT *,
+            ROW_NUMBER() OVER (ORDER BY (code, label)) AS row_id
+        FROM read_parquet('{path}')
+    ) t
+    JOIN {selected_ids_table_name} s
+    USING (row_id)
+    ORDER BY t.row_id
+    """
+
+    return con.execute(query).df()
+
+
+def fetch_original_data(
+    path: str,
+    n_samples: int,
+    random_state: int = None
+) -> pd.DataFrame:
+    """
+    Fetch the original parquet dataset with columns ["code", "label", "embedding"] from S3.
     Each code is sampled at least one.
 
     args:
@@ -27,107 +105,25 @@ def fetch_original_data(
     con = duckdb.connect()
     rng = np.random.default_rng(seed=random_state)
 
-    # ===============================
-    #          Check n_sample
-    # ===============================
-    code_len_query = f"""
-    SELECT COUNT(DISTINCT(code))
-    FROM read_parquet('{path}')
-    """
-
-    n_codes = con.execute(code_len_query).fetchone()[0]
-
+    n_codes = get_n_codes(con, path)
     assert n_samples >= n_codes, f"Please provide n_sample >= {n_codes}"
 
-    # ===============================
-    #       Exhaustive sampling
-    # ===============================
+    selected_ids, n_rows = exhaustive_sampling(con, path, rng)
 
-    code_row_count = con.execute(f"""
-    SELECT code, COUNT(*) AS n_rows
-    FROM read_parquet('{path}')
-    GROUP BY code
-    ORDER BY code
-    """).df()
+    final_ids = remaining_sampling(n_samples, n_codes, n_rows, selected_ids, rng)
 
-    code_row_count["k"] = code_row_count["n_rows"].apply(
-        lambda n: rng.integers(1, n + 1)
-    )
-    code_row_count["offset"] = code_row_count["n_rows"].cumsum() - code_row_count["n_rows"]
-    code_row_count["row_id"] = code_row_count["offset"] + code_row_count["k"]
-
-    selected_ids = code_row_count["row_id"].to_numpy()
-
-    # ===============================
-    #       Remaining sampling
-    # ===============================
-
-    remaining = n_samples - n_codes
-
-    # Adjust if remaining is too high
-    n_rows = code_row_count["n_rows"].sum()
-    remaining = min(n_rows, remaining)
-
-    # Sample the remaining codes
-    if remaining > 0:
-        remaining_ids = np.delete(np.arange(1, n_rows + 1), selected_ids - 1)
-        random_ids = rng.choice(remaining_ids, size=remaining, replace=False)
-        final_ids = np.concatenate([selected_ids, random_ids])
-    else:
-        final_ids = selected_ids
-
-    selected_ids = pd.DataFrame({
-        "row_id": final_ids
-    })
-
+    selected_ids = pd.DataFrame({"row_id": final_ids})
     con.register("selected_ids", selected_ids)
 
-    query = f"""
-    SELECT t.code, t.label, t.embedding
-    FROM (
-        SELECT *,
-            ROW_NUMBER() OVER (ORDER BY code) AS row_id
-        FROM read_parquet('{path}')
-    ) t
-    JOIN selected_ids s
-    USING (row_id)
-    """
-
-    sampled_df = con.execute(query).df()
+    sampled_df = retrieve_matching_rows(con, path, "selected_ids")
 
     return sampled_df
 
 
-def select_synthetic_data(
-        path: str,
-        code_list: list[str],
-        random_state: int
-        ) -> pd.DataFrame:
+def sample_code_equivalents(con, path, rng, code_counts):
     """
-    Fetch the synthetic parquet dataset with columns ["code", "embedding"] from s3.
-    Sample it with replacement according to the distribution of codes in code_list.
-
-    args:
-        fs (s3fs.S3FileSystem): filesystem (for S3)
-        path (str): path of the dataset
-        code_list (list): codes to sample (can contain duplictes)
-        random_state (int): seed for the random sampling
-
-    returns:
-        pd.DataFrame: a dataframe with columns code and embedding.
+    Sample row ids so that there is still the same counts for code as in code_counts.
     """
-    con = duckdb.connect()
-    rng = np.random.default_rng(seed=random_state)
-
-    # ===============================
-    #    Counting codes to sample
-    # ===============================
-    code_counts = Counter(code_list)
-
-    # ==============================================
-    #    Selecting corresponding labels randomly
-    # ==============================================
-
     # Retrieving number of labels per codes
     code_row_count = con.execute(f"""
     SELECT code, COUNT(*) AS n_rows
@@ -146,21 +142,36 @@ def select_synthetic_data(
     code_row_count["offset"] = code_row_count["n_rows"].cumsum() - code_row_count["n_rows"]
     code_row_count["row_id"] = code_row_count["offset"] + code_row_count["row_id"]
 
-    # Creating the table for duckdb
-    all_row_ids = code_row_count["row_id"].explode().to_frame()
+    return code_row_count["row_id"].explode().to_frame()
+
+
+def select_synthetic_data(
+    path: str,
+    code_list: list[str],
+    random_state: int
+) -> pd.DataFrame:
+    """
+    Fetch the synthetic parquet dataset with columns ["code", "embedding"] from s3.
+    Sample it with replacement according to the distribution of codes in code_list.
+
+    args:
+        fs (s3fs.S3FileSystem): filesystem (for S3)
+        path (str): path of the dataset
+        code_list (list): codes to sample (can contain duplictes)
+        random_state (int): seed for the random sampling
+
+    returns:
+        pd.DataFrame: a dataframe with columns code and embedding.
+    """
+    con = duckdb.connect()
+    rng = np.random.default_rng(seed=random_state)
+
+    code_counts = Counter(code_list)
+
+    all_row_ids = sample_code_equivalents(con, path, rng, code_counts)
+
     con.register("selected_ids", all_row_ids[["row_id"]])
 
-    query = f"""
-    SELECT t.code, t.label, t.embedding
-    FROM (
-        SELECT *,
-            ROW_NUMBER() OVER (ORDER BY code) AS row_id
-        FROM read_parquet('{path}')
-    ) t
-    JOIN selected_ids s
-    USING (row_id)
-    """
-
-    sampled_df = con.execute(query).df()
+    sampled_df = retrieve_matching_rows(con, path, "selected_ids")
 
     return sampled_df
---
+++ tests/datasets/test_data_importation.py
@@ -0,0 +1,60 @@
+from collections import Counter
+import duckdb
+import numpy as np
+import pandas as pd
+from datasets import data_importation
+
+CON = duckdb.connect()
+RNG = np.random.default_rng()
+REAL_PATH = "s3://mateom/graal/embeddings/NAF2025/original_train_cleaned.parquet"
+SYNTH_PATH = "s3://mateom/graal/embeddings/NAF2025/2026-03-16_gemma_synth.parquet"
+
+
+def test_get_n_codes():
+    data_importation.get_n_codes(CON, REAL_PATH)
+    data_importation.get_n_codes(CON, SYNTH_PATH)
+
+
+def test_original_data_sampling():
+    n_codes = data_importation.get_n_codes(CON, REAL_PATH)
+    n_samples = n_codes + 10
+
+    rng = np.random.default_rng(1)
+    selected_ids_1, n_rows_1 = data_importation.exhaustive_sampling(CON, REAL_PATH, rng)
+    final_ids_1 = data_importation.remaining_sampling(n_samples, n_codes, n_rows_1, selected_ids_1, rng)
+
+    selected_ids_1 = pd.DataFrame({"row_id": final_ids_1})
+    CON.register("selected_ids_1", selected_ids_1)
+    sampled_df_1 = data_importation.retrieve_matching_rows(CON, REAL_PATH, "selected_ids_1")
+
+    rng = np.random.default_rng(1)
+    selected_ids_2, n_rows_2 = data_importation.exhaustive_sampling(CON, REAL_PATH, rng)
+    final_ids_2 = data_importation.remaining_sampling(n_samples, n_codes, n_rows_2, selected_ids_2, rng)
+    selected_ids_2 = pd.DataFrame({"row_id": final_ids_2})
+    CON.register("selected_ids_2", selected_ids_2)
+    sampled_df_2 = data_importation.retrieve_matching_rows(CON, REAL_PATH, "selected_ids_2")
+
+    assert n_rows_1 == n_rows_2, "Issue on row counting"
+    assert np.array_equal(selected_ids_1, selected_ids_2), "Issue on RNG for exhaustive sampling"
+    assert np.array_equal(final_ids_1, final_ids_2), "Issue on RNG for remaining sampling"
+    assert sampled_df_1[["code", "label"]].equals(sampled_df_2[["code", "label"]]), "Matched samples are not the same"
+
+
+def test_sample_code_equivalents():
+    code_list = CON.execute(
+        f"""
+        SELECT DISTINCT(code)
+        FROM read_parquet('{REAL_PATH}')
+        ORDER BY code
+        """
+    ).df()["code"].to_list()
+
+    code_counts = Counter(code_list)
+
+    rng = np.random.default_rng(1)
+    all_row_ids_1 = data_importation.sample_code_equivalents(CON, SYNTH_PATH, rng, code_counts)
+
+    rng = np.random.default_rng(1)
+    all_row_ids_2 = data_importation.sample_code_equivalents(CON, SYNTH_PATH, rng, code_counts)
+
+    assert all_row_ids_1.equals(all_row_ids_2), "Issue on RNG for code equivalent sampling"
